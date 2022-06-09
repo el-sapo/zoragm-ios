bf6a405b4fe22d2664e03ab7cb246e074561a722
Federico Lagarmilla <federico.lagarmilla@gmail.com>
2022-06-08 23:32:06 -0300

    Initial Commit

---
+++ zoramania/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  zoramania
+//
+//  Created by Federico Lagarmilla on 8/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ zoramania/zoramaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoramaniaApp.swift
+//  zoramania
+//
+//  Created by Federico Lagarmilla on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct zoramaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
